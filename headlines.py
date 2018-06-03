35ff586274ff0f6d289b5a754f300048062d843b
Alexis Becker <alexis.becker@transferwise.com>
2018-06-03 21:11:03 +0100

    adding new files

--- headlines.py
+++ headlines.py
@@ -1,10 +1,29 @@
+import feedparser
 from flask import Flask
+from flask import request
+#Import to render html templates
+from flask import render_template
+
 
 app = Flask(__name__)
 
+RSS_FEEDS =  {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'fox': 'http://feeds.foxnews.com/foxnews/latest',
+             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+
 @app.route("/")
-def index():
-    return "Hello, World!"
+
+
+@app.route("/<publication>")
+#Use <> for URL to refer to variables
+def get_news(publication="bbc"):
+  feed = feedparser.parse(RSS_FEEDS[publication])
+  return render_template("home.html", articles=feed['entries'])
+
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug= True)
+
+feedparser.parse('http://feeds.bbci.co.uk/news/rss.xml')
